4c928e9fd90e1aab02af6816cfd9ea97a736c6e9
shaiksabiha02 <shaik@paithacs.com>
2026-08-06 16:33:16 +0530

    small modification

--- project-root/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import StudentRoutes from "./src/routes/Student.routes.js";
-const app = express();
-app.use(express.json());
-app.use("/students",StudentRoutes);
-const PORT = 4000;
-app.listen(PORT,()=>{
-    console.log(`Server running on port:${PORT}`);
-});
--- project-root/src/routes/Student.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {addStudent, getStudents} from "../controllers/Student.controller.js";
-
-const router=express.Router();
-router.post("/",addStudent);
-router.get("/",getStudents);
-export default router;
