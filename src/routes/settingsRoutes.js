2b3cd18c3a501247db2fae46ebf685b492795d5c
Gowri-Paithacs <vasimalla@paithacs.com>
2026-08-12 23:00:54 +0530

    updated settingsRoutes.js

--- src/routes/settingsRoutes.js
+++ src/routes/settingsRoutes.js
@@ -8,15 +8,13 @@ import {
     changeAdminPasswordController
 } from "../Controllers/settingsController.js";
 
-import { authenticate } from "../middlewares/auth.middleware.js";
-import { requireAdmin } from "../middlewares/admin.middleware.js";
 
 const router = express.Router();
 
 router.get("/general", fetchGeneralSettings);
 router.put("/general", editGeneralSettings);
-router.get("/profile",authenticate,requireAdmin,fetchAdminProfile);
-router.put("/profile",authenticate,requireAdmin,editAdminProfile);
-router.put("/profile/password",authenticate,requireAdmin,changeAdminPasswordController);
+router.get("/profile",fetchAdminProfile);
+router.put("/profile",editAdminProfile);
+router.put("/profile/password",changeAdminPasswordController);
 
 export default router;
